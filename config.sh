63d27ca4460d80afb346d5a9313beede6dd84168
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2023-05-22 14:37:41 -0300

    tentativa 2

--- config.sh
+++ config.sh
@@ -8,15 +8,15 @@ cd ~
 
 # instala o oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i 's/ZSH_THEME="robbyryssell"/ZSH_THEME="candy"/g' ~/.zshrc
 
 #instala o omni
 git clone https://github.com/getomni/gnome-terminal.git
 apt install dconf-cli -y
 cd gnome-terminal && ./install.sh
-
 # instala o zinit
 sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+sed -i 's/ZSH_THEME="robbyryssell"/ZSH_THEME="candy"/g' ~/.zshrc
 echo "zinit light zdharma/fast-syntax-highlighting" >> ~/.zshrc
 echo "zinit light zsh-users/zsh-autosuggestions" >> ~/.zshrc
 echo "zinit light zsh-users/zsh-completions" >> ~/.zshrc
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt-get update -y
 apt-get upgrade -y
 
 # instala os pacotes
-apt-get install git gcc g++ python3 python3-pip -y
+apt-get install make gcc g++ python3 python3-pip vim neovim-y
 
 # instala o zsh
 apt install zsh curl -y
